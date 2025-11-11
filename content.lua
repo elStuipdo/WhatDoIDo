87657bbab5019f33620319b066550145762176a2
elStuipdo <160574607+elStuipdo@users.noreply.github.com>
2025-11-11 14:10:36 +0200

    Fix print statement and connect event handler

--- content.lua
+++ content.lua
@@ -1,18 +1,21 @@
 harrastuksia = true
 if harrastus == true then
-	print("Minulla on harrastuksia)
+	print("Minulla on harrastuksia")
 else
 	print("Ei ole harrastuksia")
+end
 
 
 harrastus = script.Parent.StringValue.Value
 if harrastus == "Legojen keräily" then
       print ("Legojen keräily")
+end
 function Ulkoilu()
 	if game.Universe.Maapallo.Weather.Value == "Sunny" then
           print("Menen ulos ja otan valokuvia")
         else
           print("Olen sisällä ja olen tietokoneella tai rakentelen.")
+		end
 end
 
-script.parent.Parent.Players.Robban.Thoughts.Changed:Connect(Ulkoilu)
+script.Parent.Parent.Players.Robban.Thoughts.Changed:Connect(Ulkoilu)
